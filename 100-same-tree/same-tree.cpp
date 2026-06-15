743be9f3c1f42c2ba1b4b6992018589b46e933a4
Arpit Pandey <100606591+Arpit000009@users.noreply.github.com>
2026-06-16 01:35:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (77.18%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,35 +11,14 @@
  */
 class Solution {
 public:
-
-    void allnodes(TreeNode*p,vector<int>&vec){
-        if(p==nullptr){
-            vec.push_back(INT_MIN);
-            return;
-        }
-        vec.push_back(p->val);
-        allnodes(p->left,vec);
-        allnodes(p->right,vec);
-    }
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int>vec1;
-        vector<int>vec2;
+        if(p==nullptr || q==nullptr){
+            return p==q;
+        }
 
-        allnodes(p,vec1);
-        allnodes(q,vec2);
-        int i = 0;
-        int j = 0;
-        if(vec1.size()!=vec2.size()){
-            return false;
-        }
-        while(i<vec1.size() || j<vec2.size()){
-            if(vec1[i]!=vec2[j]){
-                return false;
-            }
-                i++;
-                j++;
-        }
-        return true;
+        bool isleft = isSameTree(p->left,q->left);
+        bool isright = isSameTree(p->right,q->right);
+
+        return isleft && isright && p->val==q->val;
     }
 };
