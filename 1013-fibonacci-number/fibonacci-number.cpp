08bcbe157e4406d5fa928d637854d20f5564703d
Arpit Pandey <100606591+Arpit000009@users.noreply.github.com>
2026-07-10 17:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
+
+    unordered_map<int,int> dp;
+
     int fib(int n) {
         if(n==0 || n==1){
             return n;
         }
 
-        return fib(n-1) + fib(n-2);
+        if(dp.find(n)!=dp.end()){
+            return dp[n];
+        }
+
+        int a1 = fib(n-1);
+        int a2 = fib(n-2);
+
+        dp[n] = a1 + a2;
+        return a1 + a2;
+        
     }
 };
