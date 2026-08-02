7f77bc38c9b6c632f13d422a57f6acdfbc35fbf8
Arpit Pandey <100606591+Arpit000009@users.noreply.github.com>
2026-08-02 05:48:17 +0530

    Time: 163 ms (5.07%) | Memory: 87.4 MB (5.01%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int ans;
         map<int,bool>mpp;
         for(int i=1;i<=nums.size()+1;i++){
             mpp[i] = false;
         }
-       
-
-        for(int i=0;i<nums.size();i++){
-            if(mpp.find(nums[i])!=mpp.end()){
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]>=1 && nums[i]<=nums.size()+1){
                 mpp[nums[i]] = true;
             }
         }
