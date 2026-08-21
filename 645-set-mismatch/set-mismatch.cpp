9c731a3e7cf9b9ef1b44de1c5d3c58c85739fefd
Arpit Pandey <100606591+Arpit000009@users.noreply.github.com>
2026-08-22 04:36:32 +0530

    Time: 63 ms (10.00%) | Memory: 36.7 MB (5.74%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+
+        map<int,int>mpp;
+
+        for(int i=1;i<=n;i++){
+            mpp[i] = 0;
+        }
+
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        vector<int>vec(2,0);
+        for(auto it:mpp){
+            if(it.second == 2){
+                vec[0] = it.first;
+            }
+            if(it.second == 0){
+                vec[1] = it.first;
+                cout<<it.first;
+            }
+        }
+
+        return vec;
+    }
+};
