6f933e961a61dbd73b0df398008262ef500c7cc2
Arpit Pandey <100606591+Arpit000009@users.noreply.github.com>
2026-08-22 05:05:56 +0530

    Time: 31 ms (20.71%) | Memory: 34.8 MB (10.96%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
 
-        map<int,int>mpp;
+        unordered_map<int,int>mpp;
 
         for(int i=1;i<=n;i++){
             mpp[i] = 0;
