6c2c59f8e4497cc4bd59c8e0c2c601fa366c8346
Arpit Pandey <100606591+Arpit000009@users.noreply.github.com>
2026-08-23 19:59:31 +0530

    Time: 82 ms (30.00%) | Memory: 146.4 MB (57.14%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>mpp;
 
-        unordered_map<int, int> mp;
-
-        // remainder 0 exists before the array starts
-        mp[0] = -1;
-
+        mpp[0] = -1;
         int sum = 0;
-
-        for(int i = 0; i < nums.size(); i++) {
-
+        for(int i=0;i<nums.size();i++){
             sum += nums[i];
-
-            int rem = sum % k;
-
-            if(mp.find(rem) != mp.end()) {
-
-                // Make sure subarray length >= 2
-                if(i - mp[rem] >= 2) {
+            int rem = sum%k;
+            if(mpp.find(rem)!=mpp.end()){
+                if(i-mpp[rem]>=2){
                     return true;
                 }
             }
-            else {
-                // Store only the FIRST occurrence
-                mp[rem] = i;
+            else{
+
+            mpp[rem] = i;
             }
         }
 
