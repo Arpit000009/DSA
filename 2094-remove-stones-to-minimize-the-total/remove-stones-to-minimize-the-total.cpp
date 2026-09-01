83b03d7febf41c117fdd8f19b24f3eb3fea59e50
Arpit Pandey <100606591+Arpit000009@users.noreply.github.com>
2026-09-02 04:27:44 +0530

    Time: 296 ms (42.67%) | Memory: 108.9 MB (7.27%) - LeetSync

---
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int>pq;
+
+        for(int i=0;i<piles.size();i++){
+            pq.push(piles[i]);
+        }
+
+        while(k){
+            int num = pq.top();
+            pq.pop();
+            num -= (num/2);
+            pq.push(num);
+            k--;
+        }
+        int ans = 0;
+        while(!pq.empty()){
+            ans+=pq.top();
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
